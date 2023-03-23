3a7a78f7d0a6b0651c956949c206adf87b57a4ea
Matthew Lin <mlin367@yahoo.com>
2023-03-23 14:27:33 -0700

    Add socket code MVP for server side

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import { Server } from 'socket.io';
 
 const app = express();
 const DEFAULT_PORT = 8080;
@@ -14,6 +15,20 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../client/dist/index.html'));
 });
 
-app.listen(DEFAULT_PORT, () =>
+const server = app.listen(DEFAULT_PORT, () =>
   console.log(`app is listening on port ${DEFAULT_PORT}`)
 );
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => console.log('a user disconnected'));
+
+  socket.on('chat message', (msg: string) => {
+    io.emit('chat message', msg);
+  });
+});
+
+export default server;
